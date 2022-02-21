3147ddeda302a4a63c5cae91775535d204882bd6
castleroad95 <68772019+castleroad95@users.noreply.github.com>
2022-02-22 00:22:44 +0900

    이성길 상권으로 움직이는 통신 해봅시다
    test1/static/data 에 .csv파일 넣으시면 됩니다.

---
+++ 장고/test1/test1/templatetags/makechart.py
@@ -0,0 +1,12 @@
+from django import template
+
+register=template.Library()
+
+@register.filter
+def ranges(n):
+    return range(0,n)
+    
+@register.simple_tag
+def pick_data(list,k,i,j):
+    if (i*k)+j<len(list):
+        return list[(i*k)+j]
